20c101b4ec9b5b57d39abe4e4f627c845ad46da9
eraybuyukkanat <erybykknt@gmail.com>
2023-12-16 22:18:48 +0300

    taking photos & managing permissions

---
+++ components/Places/ImagePicker.js
@@ -0,0 +1,49 @@
+import { Alert, Button, View } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
+
+function ImagePicker() {
+  const [cameraPermissionStatus, requestPermission] = useCameraPermissions();
+
+  async function verifyPermissions() {
+    if (cameraPermissionStatus.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionStatus.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient permission!",
+        "You need to grant camera permissions to use this app."
+      );
+      return false;
+    }
+    return true;
+  }
+
+  async function takeImageHandler() {
+    const hasPermission = await verifyPermissions();
+    if (!hasPermission) {
+      return;
+    }
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    console.log(image);
+  }
+
+  return (
+    <View>
+      <View></View>
+      <Button title="Take Image" onPress={takeImageHandler} />
+    </View>
+  );
+}
+
+export default ImagePicker;
--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,9 +1,45 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import { Colors } from "../../contants/colors";
+import ImagePicker from "./ImagePicker";
+function PlaceForm() {
+  const [enteredTitle, setEnteredTitle] = useState("");
 
-function PlaceForm(){
-    return <View>
-        <Text>Place form</Text>
-    </View>
+  function changeTitleHandler(enteredText) {
+    setEnteredTitle(enteredText);
+  }
+
+  return (
+    <ScrollView style={styles.form}> 
+      <View>
+        <Text style={styles.label}>Title</Text>
+        <TextInput style={styles.input} onChangeText={changeTitleHandler} value={enteredTitle} />
+      </View>
+      <ImagePicker />
+
+    </ScrollView>
+  );
 }
 
 export default PlaceForm;
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: "bold",
+    marginBottom: 4,
+    color: Colors.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: Colors.primary100,
+  },
+});
